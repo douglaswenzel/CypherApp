adf5d79c5f73b4748d998b585780bef8039b0e76
douglaswenzel <douglas.wenzel@hotmail.com>
2026-05-09 20:37:19 -0300

    feat: adicionar arquivo de tema com paleta de cores

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  background: '#0F1A14',
+  surface: '#17241C',
+  surfaceLight: '#203126',
+
+  primary: '#5DBB63',
+  primaryDark: '#3F8F47',
+
+  text: '#F5F7F5',
+  textSecondary: '#B7C4BA',
+
+  border: '#2D4635',
+  error: '#D96C6C',
+};
